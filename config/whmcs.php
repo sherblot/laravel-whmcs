3f6ad9f6abdf7baa51c2f8085a499db874cb9d99
Kevin Krummnacker <kevin.krummnacker@gmail.com>
2021-02-22 22:03:16 +0100

    feat: add new php-whmcs-api lib (wip)

--- config/whmcs.php
+++ config/whmcs.php
@@ -4,47 +4,40 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | HTTP Driver
+    | Default Connection
     |--------------------------------------------------------------------------
     |
-    | Supported: "guzzlehttp"
+    | Define your default whmcs connection
+    |
     */
 
-    'driver' => 'guzzlehttp',
+    'default' => 'primary',
 
     /*
     |--------------------------------------------------------------------------
-    | API Credentials
+    | WHMCS Connections
     |--------------------------------------------------------------------------
     |
-    | Enter the unhashed variant of your password if you use 'password' as 'auth_type'
+    | Define your whmcs connections here.
     |
-    | Supported auth types': "api", "password"
+    | Methods: "password", "token"
     |
     */
 
-    'auth_type' =>  env('WHMCS_AUTH_TYPE', 'password'),
+    'connections' => [
 
-    'apiurl' => env('WHMCS_API_URL', 'https://url.to.whmcs.tld/whmcs/includes/api.php'),
+        'main' => [
+            'url' => env('WHMCS_API_URL', 'https://url.to.whmcs.tld/whmcs'),
+            'method' => env('WHMCS_AUTH_TYPE', 'password'),
+            'username' => env('WHMCS_USERNAME', 'YOUR_USERNAME'),
+            'password' => env('WHMCS_PASSWORD', 'YOUR_PASSWORD'),
+        ],
 
-    'api' => [
-        'identifier' => env('WHMCS_API_IDENTIFIER', 'YOUR_API_IDENTIFIER'),
-        'secret' => env('WHMCS_API_SECRET', 'YOUR_API_SECRET'),
-    ],
-
-    'password' => [
-        'username' => env('WHMCS_USERNAME', 'YOUR_USERNAME'),
-        'password' => env('WHMCS_PASSWORD', 'YOUR_PASSWORD'),
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | ResponseType
-    |--------------------------------------------------------------------------
-    |
-    | Supported auth types': "json", "xml"
-    |
-    */
-
-    'responsetype' => env('WHMCS_RESPONSE_TYPE', 'json'),
+        'secondary' => [
+            'url' => env('WHMCS_API_URL', 'https://url.to.whmcs.tld/whmcs'),
+            'method' => env('WHMCS_AUTH_TYPE', 'token'),
+            'identifier' => env('WHMCS_API_IDENTIFIER', 'YOUR_API_IDENTIFIER'),
+            'secret' => env('WHMCS_API_SECRET', 'YOUR_API_SECRET'),
+        ]
+    ]
 ];
